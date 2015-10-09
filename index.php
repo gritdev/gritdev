bb631112defe423f0a0620412c6a8e817cc9da4b
vikramgrewal <vik.grewal1992@gmail.com>
2015-10-08 17:48:30 -0700

    Uploaded header starting index page after

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<?php
+		include 'bootstrap.php';
+		include 'header.php';
+	?>
+</head>
+
+<body>
+
+	<div class="container">
+		<div class="col-sm-6">
+		
+		</div>
+	</div>
+</body>
+
+</html>
